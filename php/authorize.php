aaa7aa306fa3918d22ad2b5d4890ef9b2e39059f
IdiosGame <36539661+HuySan@users.noreply.github.com>
2021-11-07 16:47:13 +0300

    Corrections

--- php/authorize.php
+++ php/authorize.php
@@ -1,19 +1,18 @@
 <?php
 function auth_check(){
+	session_start();
 	$value = true;
 	require_once('connection_with_bd.php');
 	if(isset($_POST['btn1']) && !empty($_POST['auth_mail']) && !empty($_POST['auth_password'])){
 		$res = mysqli_query($connect,"SELECT `mail`,`password` FROM `form` WHERE 1");
 		$res = mysqli_fetch_all($res,MYSQLI_ASSOC);
-		
+
 		foreach($res as $item){
 			if($item['mail'] == $_POST['auth_mail'] && $item['password'] == $_POST['auth_password']){
-				session_start();
 				$_SESSION['mail'] = $_POST['auth_mail'];
-				$_SESSION['pass'] = $_POST['auth_password'];
+				$_SESSION['password'] = $_POST['auth_password'];
 				$value = false;
 				header('location: ./home.html');
-				die();
 			}
 		}
 		if($value){
